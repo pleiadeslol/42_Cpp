3eccc91dd6bc947e93ab437dcf8f0b8908dee886
rzarhoun <roumaissaezarhoune@gmail.com>
2025-01-05 23:41:08 +0100

    ex00

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 22:25:13 by rzarhoun          #+#    #+#             */
+/*   Updated: 2025/01/05 23:40:20 by rzarhoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i = 1;
+	int	j;
+
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (std::islower(av[i][j]))
+				av[i][j] = std::toupper(av[i][j]);
+			j++;
+		}
+		std::cout << av[i] << " ";
+		i++;
+	}
+	std::cout << std::endl;
+}
